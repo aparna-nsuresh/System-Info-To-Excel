89e052c25609fb92ac2e7f44594cc9acd58f3d11
Sanjeet Bagga <sanjeetkbagga@gmail.com>
2016-12-05 15:27:44 -0500

    Update with the user interface of the application

--- output/far.bat
+++ output/far.bat
@@ -2,3 +2,11 @@ findandreplace.vbs %COMPUTERNAME%.txt "Office 16" "Microsoft Office 2016 (Versio
 findandreplace.vbs %COMPUTERNAME%.txt "Office 15" "Microsoft Office 2013 (Version 15)"
 findandreplace.vbs %COMPUTERNAME%.txt "Office 14" "Microsoft Office 2010 (Version 14)"
 findandreplace.vbs %COMPUTERNAME%.txt "Office 12" "Microsoft Office 2007 (Version 12)"
+
+echo WScript.Echo "Your computer inventory has been gathered successfully. Press OK to exit." > fin.vbs
+echo WScript.Quit >> fin.vbs
+fin.vbs
+
+del /f fin.vbs
+
+exit
